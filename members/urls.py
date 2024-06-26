28cdd2433d7b8144fbe94cab477bd034f9e55c4e
PatrykS165451 <164647301+PatrykS165451@users.noreply.github.com>
2024-06-26 02:09:13 +0200

    Init

---
+++ members/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import SignUp, UserEditView, PasswordsChangeView
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('signup/', SignUp.as_view(), name='signup'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    #path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html'), name='password'),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html'), name='password'),
+    path('password_success', views.password_success, name='password_success'),
+
+]
